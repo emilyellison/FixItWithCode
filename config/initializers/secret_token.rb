504457ec2e791706f8767c9aeabcfe94a7c986b7
Emily Ellison <emilyellison986@gmail.com>
2012-12-30 15:31:55 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FixItWithCode::Application.config.secret_token = 'de9d7217d0bfdb1289027100bad141a3c66bd2b48c2177fed9cb2fa22e9895a1a10dee526bcb79e44cfd035c3c2cc5d90392db054a225fff14befb69ead00a77'

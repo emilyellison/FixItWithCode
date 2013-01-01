fa42a4f2a5a3cb21d5f0cd74f9872bf227401a1a
Emily Ellison <emilyellison986@gmail.com>
2013-01-01 15:35:44 -0600

    Stylize home page

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -2,7 +2,7 @@ class PublicController < ApplicationController
   
   def index
     @administrators = Administrator.all
-    @posts = Post.most_recent.limit(5)
+    @posts = Post.most_recent.limit(2)
   end
   
 end

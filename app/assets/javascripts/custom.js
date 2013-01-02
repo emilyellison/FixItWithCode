e7f977a9147fd2f7bafceedbe3a3c69c89bdb99c
Emily Ellison <emilyellison986@gmail.com>
2013-01-01 22:51:04 -0600

    Update how animation works

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -3,16 +3,16 @@ $(document).ready( function() {
 	$('#emily_profile').hide();
 	$('#neo_profile').hide();
 	
-	$("#neo").live('click', function() {
-		$("#emily").hide();
-		$(this).animate({width: '94%', opacity: 1.0, borderRadius: 4}, {setInterval: 50});
+	$("#neo_pic").live('click', function() {
+		$('#emily_pic').hide();
+		$(this).addClass('stretch');
 		$("#neo_profile").slideDown();
 	});
 	
-	$("#emily").live('click', function() {
-		$("#neo").hide();
-		$(this).animate({width: '94%', opacity: 1.0, borderRadius: 4}, {setInterval: 50});
-		$("#emily_profile").slideDown();
+	$("#emily_pic").live('click', function() {
+		$('#neo_pic').hide();
+		$(this).addClass('stretch');
+		$("#emily_profile").slideDown();		
 	});
 			
 });

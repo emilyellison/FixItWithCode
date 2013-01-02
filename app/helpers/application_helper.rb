36dc6169daf90622d3f2cc4bd5195437c5861fab
Emily Ellison <emilyellison986@gmail.com>
2013-01-01 22:09:43 -0600

    Upload images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,19 +5,19 @@ module ApplicationHelper
   end
   
   def email_icon length
-    image_tag "http://png-4.findicons.com/files/icons/989/ivista_2/128/email.png", size: "#{length}x#{length}"
+    image_tag "email.png", size: "#{length}x#{length}"
   end
   
   def github_icon length
-    image_tag('http://www.fixitwithcode.com/assets/github_profile-a1ae9e7318180bd167664d4515fe1ac0.jpeg', size: "#{length}x#{length}")
+    image_tag('github.jpeg', size: "#{length}x#{length}")
   end
   
   def linked_in_icon length
-    image_tag('http://png-5.findicons.com/files/icons/819/social_me/64/linkedin.png', size: "#{length}x#{length}")
+    image_tag('linkedin.png', size: "#{length}x#{length}")
   end
   
   def twitter_icon length
-    image_tag('http://www.fixitwithcode.com/assets/twitter_profile-5a77947135ec6cf0f348d06ed66ecf8c.png', size: "#{length}x#{length}")
+    image_tag('twitter.png', size: "#{length}x#{length}")
   end
   
   def phone_number_link text

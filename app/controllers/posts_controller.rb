b78282970e16ef0ebc6bc6fe960d2ab751c7de00
Emily Ellison <emilyellison986@gmail.com>
2013-01-05 16:53:03 -0600

    Fix truncation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   
   def index
-    @posts = Post.most_recent.approved.page(params[:page]).per(2)
+    @posts = Post.most_recent.approved.page(params[:page]).per(3)
   end
   
   def show
